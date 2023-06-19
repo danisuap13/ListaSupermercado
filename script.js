3b1ae8e61fb5aefe62edabeb1cc1f005b27ea5ff
Daniel Suarez <est.daniel.suarez1@unimilitar.edu.co>
2023-06-19 11:59:48 -0500

    Proyecto desde 0

---
+++ script.js
@@ -0,0 +1,12 @@
+
+    let item = document.getElementById('item');
+    let precio = document.getElementById('precio');
+    let cantidad = document.getElementById('cantidad');
+
+    let array = {
+    item: item,
+    precio: precio,
+    cantidad: cantidad,
+    }   
+
+
